faef51e7133a46d7b6733f4d88746c09c0701d47
DiArch <d.yendaltsev@gmail.com>
2021-01-31 11:59:32 +0600

    Class program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_HW_SPM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, i;
+            Console.Write("Введите натуральное число не меньше двух: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            for (i = 2; i * i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
